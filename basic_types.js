6f78612db9e18af24be2acdd1b2af77312817212
wiam <naamawiam@gmail.com>
2023-06-03 12:13:15 +0200

    Add basic types

---
+++ basic_types.js
@@ -0,0 +1,6 @@
+var num1 = 5;
+var lastName = "Wiam";
+var isV = false;
+// For accept all types we will use any
+var whatEver = "wiam";
+whatEver = 5;
---
+++ basic_types.ts
@@ -0,0 +1,8 @@
+let num1: number = 5;
+let lastName: string = "Wiam";
+let isV: boolean = false;
+
+// For accept all types we will use any
+
+let whatEver: any = "wiam";
+whatEver = 5;
